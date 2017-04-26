d8211508eb4d9c3659f7b66312981de3c611c8a2
Johannes Schumeth <johannes.schumeth@gmail.com>
2017-04-26 10:57:22 +0200

    time

--- public/javascript/move.js
+++ public/javascript/move.js
@@ -1,5 +1,4 @@
-
-
+var timestamp_start = new Date();
 
 function move (player, direction) {
 
@@ -51,14 +50,27 @@ function move (player, direction) {
 }
 
 document.onkeydown = function(event) {
-    if (event.keyCode == 65) { move(1,'left'); }
-    if (event.keyCode == 87) { move(1,'up'); }
-    if (event.keyCode == 68) { move(1,'right'); }
-    if (event.keyCode == 83) { move(1,'down'); }
-    if (event.keyCode == 37) { move(2,'left'); }
-    if (event.keyCode == 38) { move(2,'up'); }
-    if (event.keyCode == 39) { move(2,'right'); }
-    if (event.keyCode == 40) { move(2,'down'); }
+    checkTime();
+    if (event.keyCode == 65) { if(checkTime) { move(1,'left'); } }
+    if (event.keyCode == 87) { if(checkTime) { move(1,'up'); } }
+    if (event.keyCode == 68) { if(checkTime) { move(1,'right'); } }
+    if (event.keyCode == 83) { if(checkTime) { move(1,'down'); } }
+    if (event.keyCode == 37) { if(checkTime) { move(2,'left'); } }
+    if (event.keyCode == 38) { if(checkTime) { move(2,'up'); } }
+    if (event.keyCode == 39) { if(checkTime) { move(2,'right'); } }
+    if (event.keyCode == 40) { if(checkTime) { move(2,'down'); } }
+}
+
+function checkTime() {
+    var timestamp_end = new Date();
+    console.log(timestamp_end - timestamp_start);
+    if ((timestamp_end - timestamp_start) > 10000) {
+        timestamp_start = new Date();
+        return true;
+    }
+    else {
+        false;
+    }
 }
 
 function getPlayerPosition (player) {
