dd79c4fdbb92c141484617bb56d20437fed4f289
itpprojektcraftstore <itpprojekt2016@gmail.com>
2017-06-02 12:02:30 +0200

    single html page

--- public/javascript/drawHTML.js
+++ public/javascript/drawHTML.js
@@ -1,14 +1,37 @@
 $(document).ready(function(){
-    $('#newGameBtn').click(function newGame(){
+    $( "body" ).delegate( "#newGameBtn", "click", function newGame() {
         var $div = $("<div class=\"border container\"></div>");
         $($div).attr('id', 'Game' + 'GameNumber');
         $('#games').append($div);
         var $name = $("<p style=\"display:inline;\">Game" + 'GameNumber' + "</p>");
         $('#Game' + 'GameNumber').append($name);
         var $btn = $("<button class=\"btn\" style=\"float:right;\">Join Game</button>")
+        $($btn).attr('id', 'joinGameBtn');
         $('#Game' + 'GameNumber').append($btn);
         var $count = $("<p style=\"float:right;\">X/Y Player</p>");
         $('#Game' + 'GameNumber').append($count);
         $('#games').append("<br>");
     });
+
+    $( "body" ).delegate( "#loginBtn", "click", function newGame() {
+        $( "#view" ).load( "browse.html #games" );
+        $('#login').load( 'blank.html');
+    });
+
+    $( "body" ).delegate( "#testBtn", "click", function includeBrowse() {
+        $('#view').load( 'blank.html');
+    });
+
+    $( "body" ).delegate( "h1", "click", function includeBrowse() {
+        $('#view').load( 'login.html');
+    });
+
+     $( "body" ).delegate( "#joinGameBtn", "click", function includeBrowse() {
+        $('#view').load( 'lobby.html');
+    });
+
+    $( "body" ).delegate( "#p1ready", "click", function includeBrowse() {
+        $('#view').load( 'gameplay_test.html');
+    });
+
 });
